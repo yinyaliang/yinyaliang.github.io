7711feff347405f4ce0a27ae26ace9c4e5249fe0
yinyaliang <yinyaliang@gmail.com>
2022-08-14 17:46:28 +0800

    #

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,7 @@
 var store = [{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
-        "tags": ["content","css","edge case","lists","markup"],
-        "url": "/edge%20case/edge-case-nested-and-mixed-lists/",
-        "teaser": null
-      },{
         "title": "Layout: Post with Nested Table of Contents",
         "excerpt":"Tests table of contents with multiple levels to verify indentation is readible. Enim laboris id ea elit elit deserunt yum -y bzip2 autoconf automake libtool glib2-devel libxml2-devel bzip2-devel libtool-ltdl-devel asciidoc libuuid-devel psmisc [[ -d /backup/soft ]] || mkdir -p /backup/soft wget https://dl.eeo-inc.com/software/heartbeat/0a7add1d9996.tar.bz2 -q -P /backup/soft/ wget https://dl.eeo-inc.com/software/heartbeat/v3.9.6.tar.gz -q -P /backup/soft/...","categories": [],
         "tags": ["table of contents"],
         "url": "/edge-case-no-yaml-title/",
-        "teaser": null
-      },{
-        "title": "Markup: Image Alignment",
-        "excerpt":"SSH 隧道或 SSH 端口转发可以用来在客户端和服务器之间建立一个加密的 SSH 连接，通过它来把本地流量转发到服务器端，或者把服务器端流量转发到本地。比如从本地访问服务器上的 MySQL 管理后台，或者把本地的串流、SMB、CIFS 等服务暴露在服务器所在的公网。本文将介绍 SSH 隧道的本地端口转发、远程端口转发等使用方式，以及如何配置 SSH 允许长连接、开机时自动建立连接。 Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item ordered item Ordered...","categories": ["Markup"],
-        "tags": ["SSH","端口转发","SOCKS"],
-        "url": "/markup/markup-image-alignment/",
         "teaser": null
       }]
