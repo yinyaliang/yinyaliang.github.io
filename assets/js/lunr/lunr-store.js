c747ff6dfd61cd47febcc1d26872a6d659c7419f
yinyaliang <yinyaliang@gmail.com>
2022-08-14 21:57:13 +0800

    #

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["table of contents"],
         "url": "/edge-case-no-yaml-title/",
         "teaser": null
+      },{
+        "title": "Layout: Post with Nested Table of Contents",
+        "excerpt":"Tests table of contents with multiple levels to verify indentation is readible. Enim laboris id ea elit elit deserunt yum -y bzip2 autoconf automake libtool glib2-devel libxml2-devel bzip2-devel libtool-ltdl-devel asciidoc libuuid-devel psmisc [[ -d /backup/soft ]] || mkdir -p /backup/soft wget https://dl.eeo-inc.com/software/heartbeat/0a7add1d9996.tar.bz2 -q -P /backup/soft/ wget https://dl.eeo-inc.com/software/heartbeat/v3.9.6.tar.gz -q -P /backup/soft/...","categories": [],
+        "tags": ["table of contents"],
+        "url": "/edge1-case-no-yaml-title/",
+        "teaser": null
       }]
