e5656c8efb89a1f84890a23a1917a2ab634e12ab
yinyaliang <yinyaliang@gmail.com>
2022-08-19 15:10:58 +0800

    #

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -82,4 +82,10 @@ var store = [{
         "tags": ["Kubernetes"],
         "url": "/kubernetes-kubeadm-update/",
         "teaser": null
+      },{
+        "title": "pgsql监控",
+        "excerpt":"测试pgsql监控 系统 Centos 7 安装pgsql sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm sudo yum install -y postgresql12-server sudo /usr/pgsql-12/bin/postgresql-12-setup initdb sudo systemctl enable postgresql-12 sudo systemctl start postgresql-12 数据库设置 sudo su postgres psql CREATE USER zbx_monitor WITH PASSWORD 'monitor' INHERIT; GRANT EXECUTE ON FUNCTION pg_catalog.pg_ls_dir(text) TO zbx_monitor; GRANT EXECUTE ON FUNCTION pg_catalog.pg_stat_file(text)...","categories": [],
+        "tags": ["Monitor"],
+        "url": "/monitor-pgsql/",
+        "teaser": null
       }]
