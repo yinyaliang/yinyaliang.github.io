5e9272228bf2f8f9506ecfabc608a9505fb0edaa
yinyaliang <yinyaliang@gmail.com>
2023-03-15 17:13:21 +0800

    #

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -311,9 +311,21 @@ var store = [{
         "url": "/monitor-zabbix-histogram-quantile/",
         "teaser": null
       },{
+        "title": "华为AC的监控数据采集",
+        "excerpt":"获取华为AC 用户数量 cpu 内存 供电等信息    ","categories": [],
+        "tags": ["Monitor"],
+        "url": "/monitor-zabbix-huawei-ac/",
+        "teaser": null
+      },{
         "title": "zabbix 报警显示网络设备的端口描述",
         "excerpt":"zabbix 报警显示网络设备的端口描述   配置自动发现   自动发现OID  discovery[{#SNMPVALUE},IF-MIB::ifDescr,{#IFALIAS},IF-MIB::ifAlias]  {#SNMPVALUE} 表示 端口描述   {#IFALIAS}   表示 端口别名   获取后的值为   [      {         \"{#SNMPINDEX}\":\"11\",         \"{#SNMPVALUE}\":\"Eth-Trunk47\",         \"{#IFALIAS}\":\"to-glb-hvn7-up\"     },     {         \"{#SNMPINDEX}\":\"12\",         \"{#SNMPVALUE}\":\"GigabitEthernet1/0/5\",         \"{#IFALIAS}\":\"test02\"     }  ]   trigger name  端口 {#SNMPVALUE} Down   触发器Message配置  \"端口描述\":\"{ITEM.LASTVALUE2}\",   效果    ","categories": [],
         "tags": ["Monitor"],
         "url": "/monitor-zabbix-network-describe/",
+        "teaser": null
+      },{
+        "title": "zabbix API实现多参数搜索",
+        "excerpt":"zabbix API实现多参数搜索   提交结构  {      \"jsonrpc\":\"2.0\",     \"method\":\"host.get\",     \"params\":{         \"output\":[             \"host\",             \"hostid\",             \"status\"         ],         \"search\":{             \"name\":[                 \"test1\",                 \"test2\"             ]         },         \"searchByAny\":\"true\"     },     \"auth\":\"daf761af769c1706f3db2919e6d157a9f90c0b9c6f0b7fe176e25d791b64295b\",     \"id\":1  }    解释  当使用多个搜索条件时，需要指定 searchByAny 参数以控制匹配的方式。  默认情况下，如果不指定 searchByAny 参数，则 Zabbix API 会将多个搜索条件视为“任何条件匹配”（即匹配任何一个条件即可返回主机）。如果您想要匹配所有条件，可以将 searchByAny 参数设置为 false，这样只有同时匹配所有条件的主机才会被返回。  上面示例请求，将使用 search 参数来搜索名称包含字符串“test1”或“test2”的所有主机，并将 searchByAny 参数设置为true，这样匹配任意一个条件的主机都会被返回  ","categories": [],
+        "tags": ["Monitor"],
+        "url": "/monitor-zabbix-search-multiple/",
         "teaser": null
       }]
